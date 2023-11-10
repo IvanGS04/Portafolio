4b0ebd3b652a1fe47537719820acb7d780eb0801
Ivan <iagsivan15@gmail.com>
2023-11-10 11:17:54 -0600

    Creacion de pagina sobre mi y contacto

--- JS/Index.js
+++ JS/Index.js
@@ -1,3 +1,4 @@
+//Funcionalidad del loader
 window.addEventListener("load", function(){
     this.document.getElementById("loader").classList.toggle("loader2");
 });
@@ -10,14 +11,13 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[2].classList.add("office");
+        habilidades[3].classList.add("cmasmas");
+        habilidades[4].classList.add("ccharp");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
     }
 }
 
