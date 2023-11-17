51e393689206fd13238298284e774a7972d7a027
Ivan <iagsivan15@gmail.com>
2023-11-16 23:59:01 -0600

    nuevo diseño

--- JS/Index.js
+++ JS/Index.js
@@ -3,30 +3,20 @@ window.addEventListener("load", function(){
     this.document.getElementById("loader").classList.toggle("loader2");
 });
 
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+//detecto el scrolling para aplicar la animación del la barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
+
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("office");
-        habilidades[3].classList.add("cmasmas");
-        habilidades[4].classList.add("ccharp");
-        habilidades[5].classList.add("javaB");
-        habilidades[6].classList.add("phy");
-        habilidades[7].classList.add("css");
-        habilidades[8].classList.add("comunicacion");
-        habilidades[9].classList.add("trabajo");
-        habilidades[10].classList.add("creatividad");
-        habilidades[11].classList.add("dedicacion");
-       
-
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("c").classList.add("barra-progreso4");
+        document.getElementById("scharp").classList.add("barra-progreso5");
+        document.getElementById("ph").classList.add("barra-progreso6");
     }
-}
 
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+}
